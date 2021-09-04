a17b099f7ffc4273a6c37f2364d52640703f0bfe
Diluka Wittahachchige <diluka.99999@outlook.com>
2021-09-04 18:26:43 +0800

    --Install BenchmarkDotNet - performance analysis --Create domain models

---
+++ BookResellerStore.Common/Domain/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookResellerStore.Common.Domain
+{
+    public class Book
+    {
+       
+    }
+}
